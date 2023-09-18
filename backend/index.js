7ba3c60e94437154f71d2173aeef4e115b9243a0
sebaei <47517850+sebaei@users.noreply.github.com>
2023-09-19 00:27:00 +0300

    Add routes in a seperate file

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,26 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send("Welcome to MERN Bookstore");
+  return response.status(234).send("Welcome To MERN Stack Tutorial");
 });
 
-// Route to save a book
-app.post("./books", async (request, response) => {
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response.status(400).send({ message: "Send all required field" });
-    }
-    const newBook = {
-      title: request.body.title,
-      author: request.body.author,
-      publishYear: request.body.publishYear,
-    };
-
-    const book = await Book.create(newBook);
-    return response.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
   .then(() => {
     console.log("App connected to database");
     app.listen(PORT, () => {
-      console.log(`App listening to port: ${PORT}`);
+      console.log(`App is listening to port: ${PORT}`);
     });
   })
   .catch((error) => {
