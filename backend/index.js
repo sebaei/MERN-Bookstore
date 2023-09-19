fc52f86038188023c2d0e320097a975d9b2ce627
sebaei <47517850+sebaei@users.noreply.github.com>
2023-09-19 19:10:27 +0300

    Update client side for better user experience

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+app.use(cors());
 
 app.get("/", (request, response) => {
   console.log(request);
